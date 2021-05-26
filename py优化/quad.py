1528de2657e1e18dd7877832c178e160744fa104
conanxu1 <872144755@qq.com>
2021-05-26 12:28:13 +0800

    ok

---
+++ py优化/quad.py
@@ -0,0 +1,27 @@
+##-*- coding: utf-8 -*- 
+# conda install cvxopt
+
+
+from cvxopt import matrix, solvers
+import numpy as np
+
+
+
+
+
+Q =matrix([[4.0, 1.0], [1.0, 2.0]])
+p = matrix([1.0,1.0],(2,1))
+G = matrix([[-1.0,0.0],[0.0,-1.0]],(2,2),tc='d')
+h = matrix([0.0,0.0])
+A = matrix( [1.0, 1.0], (1,2)) 
+#1行2列
+
+b = matrix(1.0)
+
+sol=solvers.qp(Q, p, G, h, A, b)
+
+
+print(sol['x'])
+print(sol['primal objective'])
+
+
